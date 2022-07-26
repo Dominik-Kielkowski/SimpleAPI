df2e2bbe36d05e5aa090fabaccd79328a1472b57
Dominik-Kielkowski <dominik0kielkowski@gmail.com>
2022-07-26 15:47:11 +0200

    Add project files.

---
+++ SimpleAPI/Controllers/PersonController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAPI.Data;
+using SimpleAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PersonController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Person>>> GetAllPeople()
+        {
+            return Ok(await _db.People.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
+        {
+            var person = await _db.People.FindAsync(id);
+
+            if(person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Person>>> AddPerson(Person person)
+        {
+            _db.People.Add(person);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.People.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Person>>> DeletePerson(int id)
+        {
+            var person = await _db.People.FindAsync(id);
+            
+            if(person == null)
+            {
+                return NotFound();
+            }    
+
+
+            _db.People.Remove(person);
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.People.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Person>>> EditPerson(Person person)
+        {
+            var personToEdit = await _db.People.FindAsync(person.Id);
+
+            if(personToEdit == null)
+            {
+                return NotFound();
+            }
+
+
+            personToEdit.Name = person.Name;
+            personToEdit.Age = person.Age;
+
+            await _db.SaveChangesAsync();
+
+            return Ok(await _db.People.ToListAsync());
+        }
+
+    }
+}
---
+++ SimpleAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Models;
+
+namespace SimpleAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+
+    }
+}
